d4062b9daa35b314432b01c0ab713e8e440db83d
arekbystrowicz <arek.bystrowicz@gamil.com>
2019-12-09 20:29:53 +0100

    generate spring boot project

---
+++ src/test/java/com/cafetamine/spring/data/jdbc/demo/SpringDataJdbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafetamine.spring.data.jdbc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
